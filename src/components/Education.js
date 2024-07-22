49eba5ba96f566b032528eaf9438cc81956b4230
emigithubgodz <emineimyvalajhz@gmail.com>
2024-07-22 21:48:41 +0800

    first commit

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
-import csms from '../assets/img/csms.jpg'; // Path to CSMS logo
-import sprcn from '../assets/img/sprcn.jpg'; // Path to SPRCN logo
-import spcba from '../assets/img/spcba.jpg'; // Path to SPCBA logo
-import pup from '../assets/img/pup.png'; // Path to PUP logo
+import csms from '../assets/img/csms.jpg';
+import sprcn from '../assets/img/sprcn.jpg';
+import spcba from '../assets/img/spcba.jpg';
+import pup from '../assets/img/pup.png';
+
 
 export const Education = () => {
   useEffect(() => {
@@ -38,43 +39,43 @@ export const Education = () => {
             <div className="timeline">
               <div className="timeline-item left">
                 <div className="content">
+                  <img src={csms} alt="Creative Star Montessori School" className="logo" />
                   <h4>2008-2014</h4>
                   <p>
                     Elementary School<br />
                     Creative Star Montessori School
                   </p>
                 </div>
-                <img src={csms} alt="Creative Star Montessori School" className="logo" />
               </div>
               <div className="timeline-item right">
                 <div className="content">
+                  <img src={sprcn} alt="San Pedro Relocation Center National High School" className="logo" />
                   <h4>2014-2018</h4>
                   <p>
                     Junior High School<br />
                     San Pedro Relocation Center National High School
                   </p>
                 </div>
-                <img src={sprcn} alt="San Pedro Relocation Center National High School" className="logo" />
               </div>
               <div className="timeline-item left">
                 <div className="content">
+                  <img src={spcba} alt="San Pedro College of Business Administration" className="logo" />
                   <h4>2018-2020</h4>
                   <p>
                     Senior High School<br />
                     San Pedro College of Business Administration
                   </p>
                 </div>
-                <img src={spcba} alt="San Pedro College of Business Administration" className="logo" />
               </div>
               <div className="timeline-item right">
                 <div className="content">
+                  <img src={pup} alt="Polytechnic University of the Philippines" className="logo" />
                   <h4>2020-2024</h4>
                   <p>
                     College<br />
                     Polytechnic University of the Philippines
                   </p>
                 </div>
-                <img src={pup} alt="Polytechnic University of the Philippines" className="logo" />
               </div>
             </div>
           </Col>
